26b68679db8295d3e41e9a905bf20d43d54edd2c
tyler.da55@gmail.com <tyler.da55@gmail.com>
2022-06-28 20:57:37 +1000

    mvoed icons, mouse controller

--- Assets/UI/MouseController.cs
+++ Assets/UI/MouseController.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameControllers.Services;
+using GameControllers.Models;
 using Zenject;
 
 namespace UI
 {
     public class MouseController : MonoBehaviour2
     {
+        public Sprite[] cursorTexures;
+        private CursorMode cursorMode = CursorMode.Auto;
+        private Vector2 hotSpot = Vector2.zero;
         private IUnitActionService actionService;
         [Inject]
         public void Construct(IUnitActionService _actionService)
         {
             this.actionService = _actionService;
+            this.subscriptions.Add(this.actionService.mouseAction.Subscribe(action =>
+            {
+                this.setMouseIcon(action);
+            })
+            );
         }
         // Start is called before the first frame update
         void Start()
@@ -24,6 +33,18 @@ namespace UI
         void Update()
         {
 
+        }
+
+        void setMouseIcon(eMouseAction action)
+        {
+            if (action == eMouseAction.None)
+            {
+                Cursor.SetCursor(null, this.hotSpot, this.cursorMode);
+            }
+            else
+            {
+                Cursor.SetCursor(this.cursorTexures[(int)action].texture, this.hotSpot, this.cursorMode);
+            }
         }
     }
 }
