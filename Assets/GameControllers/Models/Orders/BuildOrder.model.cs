fcc1178c7e1a01524ef10d0f9b6b6c811c5bd78b
tyler.da55@gmail.com <tyler.da55@gmail.com>
2022-07-21 00:53:53 +1000

    hide build order icon

--- Assets/GameControllers/Models/Orders/BuildOrder.model.cs
+++ Assets/GameControllers/Models/Orders/BuildOrder.model.cs
@@ -7,7 +7,7 @@ namespace GameControllers.Models
     public class BuildOrderModel : UnitOrderModel
     {
         public eBuildingType buildingType;
-        public BuildOrderModel(Vector3Int _coordinates, eBuildingType _buildingType) : base(_coordinates, eOrderTypes.Build)
+        public BuildOrderModel(Vector3Int _coordinates, eBuildingType _buildingType, bool showIcon = false) : base(_coordinates, eOrderTypes.Build, showIcon)
         {
             this.buildingType = _buildingType;
         }
