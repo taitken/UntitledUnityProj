42f51391d033109b97278affff41eb6d55d26aeb
tyler.da55@gmail.com <tyler.da55@gmail.com>
2022-11-02 11:32:29 +1100

    panel reshuffle

---
+++ Assets/UI/Panels/Controllers/BuildPanelAssetFactory.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UI.Models;
+using GameControllers.Services;
+using System.Linq;
+using System;
+
+namespace UI
+{
+    public class BuildPanelAssetFactory : MonoBehaviour2
+    {
+        public List<BasePanel> panelPrefabs;
+        public BasePanel CreatePanelWindow(RectTransform parentTransform, BasePanelModel panelWindowModel, IList<IBaseService> services)
+        {
+            if (this.panelPrefabs.Count > (int)panelWindowModel.panelType)
+            {
+                BasePanel newPanel = Instantiate(this.panelPrefabs[(int)panelWindowModel.panelType]);
+                newPanel.InjectServices(services);
+                newPanel.Construct(panelWindowModel);
+                newPanel.GetComponent<RectTransform>().SetParent(parentTransform);
+                switch (panelWindowModel.panelType)
+                {
+                    case ePanelTypes.ObjectInfo:
+                        newPanel.GetComponent<RectTransform>().anchoredPosition = new Vector3(-190, 270);
+                        break;
+                    case ePanelTypes.RecipeSelector:
+                        newPanel.GetComponent<RectTransform>().anchoredPosition = new Vector3(-413, 270);
+                        break;
+                    case ePanelTypes.SeedSelector:
+                        newPanel.GetComponent<RectTransform>().anchoredPosition = new Vector3(-413, 270);
+                        break;
+                }
+                return newPanel;
+            }
+            else
+            {
+                this.ThrowMissingPrefabError(panelWindowModel.panelType);
+                return null;
+            }
+        }
+
+        private void ThrowMissingPrefabError(ePanelTypes panelType)
+        {
+            Debug.LogException(new System.Exception("Chosen context prefab has not been added to the Context Asset Factory. Attemped type: " + panelType.ToString()));
+        }
+    }
+}
